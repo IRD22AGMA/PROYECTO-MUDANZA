e9043bcea4b76837c1755d2b412d29e565ebcfbf
IRD22AGMA <157087943+IRD22AGMA@users.noreply.github.com>
2024-04-19 13:56:38 -0500

    Proyecto Mudanza

---
+++ src/Main.kt
@@ -0,0 +1,58 @@
+fun main () {
+    var opcion: Int
+    val controlador = Controlador()
+    var mostrarMenu = true
+    do {
+        if (mostrarMenu) {
+            println("Bienvenido al sistema")
+            println("Seleccione una Opcion.")
+            println("1. Registrar solicitud de mudanza.")
+            println("2. Registrar repartidor.")
+            println("3. Registrar Cliente")
+            println("4. Mostrar Solicitudes.")
+            println("5. Mostrar Repartidores")
+            println("6. Mostrar Clientes Asociados")
+            println("7. Salir del Sistema.")
+        }
+        mostrarMenu = false
+        opcion = readln().toInt()
+        when (opcion) {
+            1 -> {
+                println("Ha seleccionado Registrar una Solicitud.")
+                if (controlador.listaRepartidores.isEmpty()) {
+                    println("No hay Repartidores en el Sistema, registre y asocie al repartidor a una Mudanza para comenzar")
+                    controlador.registrarRepartidor()
+                }
+                println("Registrando Solicitud.")
+                controlador.registrarSolicitud()
+                mostrarMenu = true
+            }
+            2 -> {
+                println("Ha seleccionado registrar un repartidor.")
+                controlador.registrarRepartidor()
+                mostrarMenu = true
+            }
+            3-> {
+                println("Ha Seleccionado registrar un Cliente.")
+                controlador.registrarCliente()
+                mostrarMenu = true
+            }
+            4->{
+                println("Mostrando Solicitudes Activas:")
+                controlador.mostrarSolicitudes()
+                mostrarMenu = true
+            }
+            5->{
+                println("Mostrando Repartidores:")
+                controlador.mostrarRepartidores()
+                mostrarMenu = true
+            }
+            6->{
+                println("Mostrando Clientes Asociados:")
+                controlador.mostrarClientes()
+                mostrarMenu = true
+            }
+            else -> println("Opcion invalida")
+        }
+    } while (opcion != 7)
+}
---
+++ src/SolicitudMudanza.kt
@@ -0,0 +1,10 @@
+class Solicitud (
+    val clienteSolicitud: String,
+    val idSolicitud: Int,
+    val repartidor: String,
+    val direccionDestino: String,
+    val fechaDeMudanza: String,
+    val totalMuebles: Int,
+    val costeMudanza: Double,
+    val valorPorMueble: Int
+)
